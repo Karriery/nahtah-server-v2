129a89f75aabb51734575849cd783512ca96e1f3
3ibsi69 <a.abbassi21306@pi.tn>
2024-02-28 15:24:44 +0100

    Update notificationsRouter.js to use double quotes for string literals

--- server/router/notificationsRouter.js
+++ server/router/notificationsRouter.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const notificationController = require('../controller/notificationsController');
+const notificationController = require("../controller/notificationsController");
 
-router.get('/:client', notificationController.getNotificationsByClient);
-router.put('/:id', notificationController.updateNotification);
-router.post('/', notificationController.createNotification);
+router.get("/:client", notificationController.getNotificationsByClient);
+router.put("/:id", notificationController.updateNotification);
+router.post("/", notificationController.createNotification);
 
 module.exports = router;
