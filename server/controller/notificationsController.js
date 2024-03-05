1766f9decf2284d438169336154d5df3f177ed28
3ibsi69 <a.abbassi21306@pi.tn>
2024-03-05 09:41:59 +0100

    Fix error handling in notificationsController.js

--- server/controller/notificationsController.js
+++ server/controller/notificationsController.js
@@ -6,7 +6,7 @@ module.exports = {
       req.io.emit("newNotification", newNotification, (error) => {
         if (error) {
           console.error("Error emitting notification:", error);
-          res.status(500).json({ error });
+          res.status(500).json({ error: "Internal server error" });
         } else {
           console.log("Notification emitted successfully");
           res.status(201).json(newNotification);
