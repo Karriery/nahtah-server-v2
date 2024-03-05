83062084f1dea636e4e7038fa3084372a1d60a53
3ibsi69 <a.abbassi21306@pi.tn>
2024-03-05 13:59:55 +0100

    Refactor newsletterController.js to simplify emit logic

--- server/controller/newsletterController.js
+++ server/controller/newsletterController.js
@@ -4,13 +4,7 @@ module.exports = {
   async createNewsletter(req, res) {
     try {
       const newsletter = await NewsletterService.create(req.body);
-      req.io.emit("newsletter", newsletter, (error) => {
-        if (error) {
-          res.status(500).json({ error: "Internal server error" });
-        } else {
-          res.status(201).json(newsletter);
-        }
-      });
+      req.io.emit("newsletter", newsletter);
     } catch (error) {
       console.error("Error creating newsletter:", error);
       res.status(500).json({ error: "Internal server error" });
