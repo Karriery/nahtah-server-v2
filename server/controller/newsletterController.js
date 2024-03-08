358b67f735e36b1656ddbdc2e46d58172420f7e0
3ibsi69 <a.abbassi21306@pi.tn>
2024-03-08 23:32:35 +0100

    Update limit value in newsletterController.js

--- server/controller/newsletterController.js
+++ server/controller/newsletterController.js
@@ -14,7 +14,7 @@ module.exports = {
     try {
       let { page, limit } = req.query;
       page = parseInt(page) || 1;
-      limit = parseInt(limit) || 10;
+      limit = parseInt(limit) || 8;
 
       const allNewsletters = await NewsletterService.getAll();
       const skip = (page - 1) * limit;
