be747ec4080cdf9ea557b5f30603bec0170579ee
3ibsi69 <a.abbassi21306@pi.tn>
2024-04-22 10:11:44 +0100

    Update MongoDB connection string in server/model/connection.js

--- server/model/connection.js
+++ server/model/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const db = mongoose.connect(
-  "mongodb+srv://halim:20028952Sami@cluster0.b1pz3.mongodb.net/nahtahtest?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+const db = mongoose.connect("mongodb://localhost/nahtaouta", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 module.exports = db;
